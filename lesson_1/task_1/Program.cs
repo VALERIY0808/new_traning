cd094bf0e9599980ad4f641a875ca6d53c5f6716
Valeriy Sobolev <vsobolev93@list.ru>
2022-05-29 20:47:06 +0400

    complete

--- lesson_1/task_1/Program.cs
+++ lesson_1/task_1/Program.cs
@@ -1,4 +1,46 @@
 ﻿// Задача 1 Напишпте программу которая на вход принимает 2 числа и выдает, какое число большее а какое меньшее
 Console.WriteLine("Первые два числа для проверки 5 и 7");
-int a = 5;
-int b = 7;
+int a = 5, b = 7;
+if (a > b)
+{
+    Console.Write("Максимальное число =");
+    Console.WriteLine(a);
+}
+else
+{
+    Console.Write("Максимальное число =");
+    Console.WriteLine(b);
+}
+Console.WriteLine("Выбор максимального числа из второго варианта (2 или 10)");
+
+a = 2;
+b = 10;
+
+if (a > b)
+{
+    Console.Write("Максимальное число =");
+    Console.WriteLine(a);
+}
+else
+{
+    Console.Write("Максимальное число =");
+    Console.WriteLine(b);
+}
+
+Console.WriteLine("Выбор максимального числа из третьего варианта (9 или 3)");
+
+a = 9;
+b = 3;
+
+if (a > b)
+{
+    Console.Write("Максимальное число =");
+    Console.WriteLine(a);
+}
+else
+{
+    Console.Write("Максимальное число =");
+    Console.WriteLine(b);
+}
+
+Console.WriteLine("Задача выполнена");
