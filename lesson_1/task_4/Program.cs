70a2b1afd5e20eff3d2cbf038b4399c2b44a178d
Valeriy Sobolev <vsobolev93@list.ru>
2022-05-29 20:55:47 +0400

    complete

--- lesson_1/task_4/Program.cs
+++ lesson_1/task_4/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число N ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+int a = 1;
+while (i < N)
+{
+    if (a % 2 == 0)
+    {
+        Console.Write("Четное число =");
+        Console.WriteLine(a);
+    }
+    a = a + 1;
+    i = i + 1;
+}
+Console.WriteLine("Задача решена");
