09826118d697a110e4288dddb58e03e02aa92706
Valeriy Sobolev <vsobolev93@list.ru>
2022-06-21 11:39:11 +0400

    Задача 23 семинар 3

---
+++ lesson_3/task_3/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Таблица квадратов: ");
+for (int i=0;i < number;i++)
+{
+    Console.Write($"{Math.Pow(i+1,3)} ");
+}
