1cd10a9be1ce6443920264aa24a2fdc960905318
Valeriy Sobolev <vsobolev93@list.ru>
2022-06-21 11:22:31 +0400

    Задача 19 семинар 3

---
+++ lesson_3/task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+Console.Write("Введите пятизначное число число:  ");
+string? num = Console.ReadLine();
+int contr = Convert.ToInt32(num);
+if (contr<10000)
+{
+    Console.WriteLine("Число должно быть пятизначным");
+}
+else
+{
+    if ((num[0]==num[4]) && (num[1]==num[3]))
+    {
+        Console.WriteLine("ДА");
+    }
+    else
+    {
+        Console.WriteLine("НЕТ");
+    }
+}
