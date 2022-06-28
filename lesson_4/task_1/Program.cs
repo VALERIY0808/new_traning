9d124506f88aacd6519bce82805fb72f934670d7
Valeriy Sobolev <vsobolev93@list.ru>
2022-06-28 20:32:50 +0400

    Задача 1 семинар 4

---
+++ lesson_4/task_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B\
+Console.WriteLine("Введите число A:");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B:");
+int B = Convert.ToInt32(Console.ReadLine());
+int contr = 1;
+for (int i = 1; i<=B;i++)
+{
+    contr = contr*A;   
+}
+Console.WriteLine($"А в натуральной степени В = {contr}");
