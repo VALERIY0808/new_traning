b0db119ca0d06e1853784c45b2db048342847e63
Valeriy Sobolev <vsobolev93@list.ru>
2022-06-30 22:51:48 +0400

    задача 2 семинар 4

---
+++ lesson_4/task_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Введите число :");
+string dig = Console.ReadLine();
+int contr = 0;
+int result = 0;
+int lng = dig.Length;
+for (int i = 0; i<lng;i++)
+{
+contr = Convert.ToInt32(dig[i].ToString());
+result = result + contr;
+}
+Console.WriteLine($"Сумма цифр в числе {dig} = {result}");
