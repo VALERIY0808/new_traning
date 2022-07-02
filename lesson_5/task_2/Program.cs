a2cc902e327568307a2a7ebcf4dd7b2a1b3575a5
Valeriy Sobolev <vsobolev93@list.ru>
2022-07-02 15:33:05 +0400

    Задача 36 семинар 5

---
+++ lesson_5/task_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+Console.WriteLine("Введите количество элементов массива:");
+int size = Convert.ToInt32(Console.ReadLine());
+int [] arr = new int[size];
+int contr = 0;
+for (int i = 0; i<size; i++)
+{
+    arr [i] = new Random().Next(-99,100);
+    Console.Write($"{arr[i]}  ");
+    if (i % 2 != 0)
+    {
+        contr+=arr[i];
+    }
+}
+Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях = {contr}");
