c4a8223cdafe5801b56ebaf50b3b06befc14aab6
Valeriy Sobolev <vsobolev93@list.ru>
2022-07-08 17:38:09 +0400

    задача 1 семинар 7

---
+++ lesson_7/task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+Console.WriteLine("Задайте размерность массива m x n:");
+Console.Write("Введите  m ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите  n ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] matrix = new double[m,n];
+for (int i = 0; i<m; i++)
+{
+    for (int j = 0; j<n; j++)
+    {
+        matrix[i,j] = new Random().NextDouble() * 20 - 10;
+        double number = Math.Round(matrix[i, j], 1);
+        Console.Write(number + " ");
+    }
+    Console.WriteLine();
+} 
